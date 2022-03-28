04dc1c866eed574e5e32c6903ee951d241c63126
Albert Tang <tangalbert919@yahoo.com>
2022-03-28 15:10:34 -0500

    Execute dos2unix before applying BSD patches

--- devutils/apply-bsd-patches.sh
+++ devutils/apply-bsd-patches.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # This script will apply all BSD patches.
+# We need to use dos2unix first. One of the patches we have requires it.
+dos2unix build/src/third_party/vulkan_memory_allocator/include/vk_mem_alloc.h
 echo "Applying BSD patches. None of them should fail."
 for i in $(ls files/patch-*)
 do
