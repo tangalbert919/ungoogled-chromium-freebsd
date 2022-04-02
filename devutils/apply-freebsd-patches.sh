2bfe6779c6006a65e04797f5dc90103b491d5cc3
Albert Tang <tangalbert919@yahoo.com>
2022-04-01 22:47:04 -0500

    Do not use dos2unix before applying FreeBSD patches
    FreeBSD doesn't do this before applying its patches anymore.

--- devutils/apply-freebsd-patches.sh
+++ devutils/apply-freebsd-patches.sh
@@ -2,7 +2,6 @@
 
 # This script will apply all BSD patches.
 # We need to use dos2unix first. One of the patches we have requires it.
-dos2unix build/src/third_party/vulkan_memory_allocator/include/vk_mem_alloc.h
 echo "Applying BSD patches. None of them should fail."
 for i in $(ls freebsd-patches/chromium/files/patch-*)
 do
