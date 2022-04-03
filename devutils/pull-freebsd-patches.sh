c959430a6157ae9e1d8903499e90822161ee2fac
Albert Tang <tangalbert919@yahoo.com>
2022-04-03 15:06:17 -0500

    Pull patches more quietly

--- devutils/pull-freebsd-patches.sh
+++ devutils/pull-freebsd-patches.sh
@@ -9,5 +9,5 @@ FREEBSD_HASH=$(grep 'FREEBSD_HASH=\t*' Makefile | sed 's/FREEBSD_HASH=\t//')
 rm -rf freebsd-patches
 # Download the new set of FreeBSD patches.
 curl -o freebsd-ports-$FREEBSD_HASH.tar.gz https://codeload.github.com/freebsd/freebsd-ports/tar.gz/$FREEBSD_HASH
-tar -xvf freebsd-ports-$FREEBSD_HASH.tar.gz freebsd-ports-$FREEBSD_HASH/www/chromium --strip-components=1
+tar -xf freebsd-ports-$FREEBSD_HASH.tar.gz freebsd-ports-$FREEBSD_HASH/www/chromium --strip-components=1
 mv www freebsd-patches
