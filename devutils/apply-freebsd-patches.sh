db0a09ef773882156d9bc5020594f33222b3286f
Albert Tang <tangalbert919@yahoo.com>
2022-06-15 12:28:57 -0500

    Rename target folder for FreeBSD upstream patches
    The folder "freebsd-patches" will contain the patches needed to patch
    the ungoogled-chromium patches. The upstream patches needed for Chromium
    to compile on FreeBSD will be downloaded to "fb-upstream-patches" now.

--- devutils/apply-freebsd-patches.sh
+++ devutils/apply-freebsd-patches.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # This script will apply all BSD patches.
-# We need to use dos2unix first. One of the patches we have requires it.
 echo "Applying BSD patches. None of them should fail."
-for i in $(ls freebsd-patches/chromium/files/patch-*)
+for i in $(ls fb-upstream-patches/chromium/files/patch-*)
 do
   #patch -d build/src -Np0 -i ../../$i --dry-run | grep FAILED
   patch -d build/src -Np0 -i ../../$i | grep rejects
