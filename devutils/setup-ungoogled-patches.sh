0a0cbb89c1c0939d519fdbbe67a1f406e834ed76
Albert Tang <tangalbert919@yahoo.com>
2022-08-20 21:13:15 -0500

    Rewrite setup-ungoogled-patches.sh
    Rather than manually update the submodule to get the latest patches from
    Ungoogled Chromium, just read the Makefile and download the correct
    version automatically instead.
    
    This approach will use more network bandwidth, but will save time on
    maintaining this port.

--- devutils/setup-ungoogled-patches.sh
+++ devutils/setup-ungoogled-patches.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # This script will setup Ungoogled-chromium patches.
-rm -rf ungoogled-patches && mkdir ungoogled-patches
-cp -r ungoogled-chromium/patches/* ungoogled-patches
+CHROMIUM_VERSION=$(grep 'PORTVERSION=\t*' Makefile | sed 's/PORTVERSION=\t//')
+UG_REVISION=$(grep 'UG_REVISION=\t*' Makefile | sed 's/UG_REVISION=\t//')
+# Clear out the current set of ungoogled-chromium patches.
+rm -rf ungoogled-patches
+# Download the new set of ungoogled-chromium patches.
+curl -o ungoogled-chromium-$CHROMIUM_VERSION-$UG_REVISION.tar.gz https://codeload.github.com/ungoogled-software/ungoogled-chromium/tar.gz/$CHROMIUM_VERSION-$UG_REVISION
+tar -xf ungoogled-chromium-$CHROMIUM_VERSION-$UG_REVISION.tar.gz ungoogled-chromium-$CHROMIUM_VERSION-$UG_REVISION/patches --strip-components=1
+mv patches ungoogled-patches
 for i in $(ls ungoogled-patches/**/**/*.patch)
 #for f in 'ungoogled-patches/**/*.patch'
 do
